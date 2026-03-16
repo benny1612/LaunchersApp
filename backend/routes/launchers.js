74d6cfddab5a58e25f28573a259cc6aa1f010508
benny1612 <bn161299@gmail.com>
2026-03-16 17:33:48 +0200

    I added an update endpoint.

--- backend/routes/launchers.js
+++ backend/routes/launchers.js
@@ -81,4 +81,29 @@ router.post("/", async (req, res) => {
   }
 });
 
+
+router.put('/:id',async(req,res)=>{
+    try{
+        const db =getDb()
+        const {id}=req.params
+     const { name, rocketType, latitude, longitude, city } = req.body;
+     const updateData ={}
+     if (name) {updateData.name=name}
+    if (rocketType) {updateData.rocketType=rocketType}
+    if(latitude){updateData.latitude=latitude}
+    if(longitude){updateData.longitude=longitude}
+    if(city){updateData.city=city}
+    if (Object.keys(updateData).length === 0) {
+  return res.status(400).json({ msg: "Nothing was updated" });}
+    const updateLauncher = await db.collection("launchers").updateOne(
+  { _id: new ObjectId(id) },
+  { $set: updateData }
+);
+res.status(200).json({msg:"filed update"})
+
+    }catch (error) {
+    res.status(500).json({ msg: "internal server error" });
+  }
+})
+
 export default router;
