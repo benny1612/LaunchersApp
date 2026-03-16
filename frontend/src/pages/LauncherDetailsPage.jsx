11b01cb8c502336499925ff3d78f9b55a330f7d9
benny1612 <bn161299@gmail.com>
2026-03-16 14:47:43 +0200

    A home page that shows all the details of the launchers and a details page that shows the details of a single launcher, and the deletion will also be done from this page later.

--- frontend/src/pages/LauncherDetailsPage.jsx
+++ frontend/src/pages/LauncherDetailsPage.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getLauncherById } from '../api.js'
 
 export default function LauncherDetailsPage() {
+  const { id } = useParams()
+  const [launcher, setLauncher] = useState()
+const loadData = async () => {
+      try {
+        const data = await getLauncherById(id)
+        setLauncher(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  useEffect(() => {
+    
+    loadData()
+  }, [])
+
   return (
-    <div>LauncherDetailsPage</div>
+    <div>
+      {launcher && (
+        <div>
+          <p> name: {launcher.name}</p>
+          <p>rocket Type: {launcher.rocketType}</p>
+          <p>City: {launcher.city}</p>
+          <p>latitude: {launcher.latitude}</p>
+          <p>longitude:{launcher.longitude}</p>
+        </div>
+      )}
+    </div>
   )
 }
