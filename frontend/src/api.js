3d7a10c0b74f24b4071b697fc44a0f52e99e19cf
benny1612 <bn161299@gmail.com>
2026-03-16 12:28:40 +0200

    add a addLauncher  axios function

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,31 @@
 import axios from "axios";
-export const getAllLaunchers = async ()=>{
-    try{
-        const response = await axios.get(`http://localhost:3000/api/launchers`)
-        return response.data
-    }
-    catch(err){
-        console.log(err)
-    }
-}
+export const getAllLaunchers = async () => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/launchers`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export const getLauncherById = async (id)=>{
-    try{
-        const response = await axios.get(`http://localhost:3000/api/launchers/${id}`)
-        return response.data
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+export const getLauncherById = async (id) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/api/launchers/${id}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const addLauncher = async (newLauncher) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:3000/api/launchers/`,
+      newLauncher,
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
