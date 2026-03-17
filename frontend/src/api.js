dfba8037b364fee373594c2a9434927957387edd
benny1612 <bn161299@gmail.com>
2026-03-17 15:24:56 +0200

    added Axios functions for all the new endpoints, and for the ones from the first part, I've now included the auth token from local storage in the headers

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+
 export const getAllLaunchers = async () => {
   try {
-    const response = await axios.get(`http://localhost:3000/api/launchers`);
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`http://localhost:3000/api/launchers`, {
+      headers: { Authorization: token },
+    });
     return response.data;
   } catch (err) {
     console.log(err);
@@ -10,19 +14,26 @@ export const getAllLaunchers = async () => {
 
 export const getLauncherById = async (id) => {
   try {
+    const token = localStorage.getItem("token");
     const response = await axios.get(
       `http://localhost:3000/api/launchers/${id}`,
+      {
+        headers: { Authorization: token },
+      },
     );
     return response.data;
   } catch (error) {
     console.log(error);
   }
 };
+
 export const addLauncher = async (newLauncher) => {
   try {
+    const token = localStorage.getItem("token");
     const response = await axios.post(
       `http://localhost:3000/api/launchers/`,
       newLauncher,
+      { headers: { Authorization: token } },
     );
     return response.data;
   } catch (error) {
@@ -30,22 +41,131 @@ export const addLauncher = async (newLauncher) => {
   }
 };
 
-export const deleteLauncher = async (id)=>{
-    try{
-        const response = await axios.delete(`http://localhost:3000/api/launchers/${id}`
-           
-
-        )
-         return response.data
-    } catch (error) {
+export const deleteLauncher = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.delete(
+      `http://localhost:3000/api/launchers/${id}`,
+      {
+        headers: { Authorization: token },
+      },
+    );
+    return response.data;
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
-export const Updatelauncher = async (id,updeteData)=>{
-    try{
-        const response = await axios.put(`http://localhost:3000/api/launchers/${id}`,updeteData)
-    }catch(error){
-        console.log(error)
-    }
-}
+export const Updatelauncher = async (id, updeteData) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.put(
+      `http://localhost:3000/api/launchers/${id}`,
+      updeteData,
+      { headers: { Authorization: token } },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (userData) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/api/auth/login",
+      userData,
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createNewUser = async (newUser) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.post(
+      "http://localhost:3000/api/auth/register/create",
+      newUser,
+      { headers: { Authorization: token } },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateUser = async (id, updateData) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.put(
+      `http://localhost:3000/api/auth/register/update/${id}`,
+      updateData,
+      { headers: { Authorization: token } },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteUser = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.delete(
+      `http://localhost:3000/api/auth/register/delete/${id}`,
+      { headers: { Authorization: token } },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getUser = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`http://localhost:3000/api/auth/getUser`, {
+      headers: { Authorization: token },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(
+      `http://localhost:3000/api/auth/getAllUsers`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+export const DestroyLauncher = async (id, isDestroyed) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.put(
+      `http://localhost:3000/api/launchers/destroyed/${id}`,
+      { destroyed: isDestroyed },
+      {
+        headers: {
+          Authorization: token
+        }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
