2cdab1a3fb083aee141e0b013dabf6f4e6a3b63a
benny1612 <bn161299@gmail.com>
2026-03-17 17:11:53 +0200

    I added a page to view all users that loads the user data when the component is rendered. I added a button that sends to the edit and delete user page next to each user.

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function AdminPage() {
   return (
-    <div>AdminPage</div>
-  )
+    <div className="flex justify-center p-8">
+      <nav >
+        <Link 
+          to="/RegisterPage" 
+          className="px-6 py-2 hover:bg-gray-100 "
+        >
+          Register Page
+        </Link>
+        
+        <Link 
+          to="/UsersPage" 
+          className="px-6 py-2 hover:bg-gray-100"
+        >
+          UsersPage
+        </Link>
+      </nav>
+    </div>
+  );
 }
